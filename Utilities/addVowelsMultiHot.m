92c7283cf1180f00d4b5f52354da700edbe075ee
anonymousresearch977 <anonymousresearch977@gmail.com>
2026-04-15 14:18:06 +0800

    Update addVowelsMultiHot.m

--- Utilities/addVowelsMultiHot.m
+++ Utilities/addVowelsMultiHot.m
@@ -1,5 +1,5 @@
 function T = addVowelsMultiHot(T)
-    % Add multi-hot encoding columns for vowels (NO NONE)
+    % Add multi-hot encoding columns for vowel /a/ (NO NONE)
     n = height(T);
     T.isA = false(n, 1);
 
